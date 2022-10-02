048b9acabc850f5f37c32ceda7ba34487216eaa7
FlamingOnionMoose <hewwhah@gmail.com>
2022-10-02 08:00:17 -0600

    Person Regirst initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import Oruga from '@oruga-ui/oruga-next'
-import '@oruga-ui/oruga-next/dist/oruga.min.css'
+import Oruga from "@oruga-ui/oruga-next";
+import "@oruga-ui/oruga-next/dist/oruga.min.css";
 
+import "@mdi/font/css/materialdesignicons.css";
 
-import './assets/styles/main.scss'
+import "@/assets/styles/_reset.css";
+import "@/assets/styles/_base.css";
+import "@/assets/styles/_animations.css";
 
-createApp(App)
-	.use(Oruga)
-	.mount('#app')
+import router from "./router";
+import VueWriter from "vue-writer";
+
+createApp(App).use(router).use(Oruga).use(VueWriter).mount("#app");
+// TODO: Introduce eslint with vue3 + typescript support
